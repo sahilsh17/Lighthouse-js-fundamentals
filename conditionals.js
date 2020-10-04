40c500410a35025b24f535ea1c6d6171623100c9
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-10-04 07:21:31 +0000

    Adding more conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,12 +1,5 @@
-const raining = true;
-const cold = true;
+let raining=true;
 
-if (raining) {
-  console.log("Don't forget your umbrella!");
-}
-
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-}
-
-console.log("Now you're ready to go outside!");
+if (!raining) {
+    console.log("Leave your umbrella at home!");
+  }
